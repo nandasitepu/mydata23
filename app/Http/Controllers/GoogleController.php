003e6cab2feda575db79798818cdfc806f259d90
unknown <nanda.sitepu@gmail.com>
2024-03-08 17:14:02 +0700

    sign in fix

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
+use Socialite;
+
 use Exception;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
